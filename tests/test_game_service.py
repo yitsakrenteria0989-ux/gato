0ffbb66b85a70cd3777eacfb143236b90d0fede5
Yitsak Rentería <Yitrenteria@corpcab.com.mx>
2026-06-25 14:30:32 -0600

    test: add move validation and turn change tests

--- tests/test_game_service.py
+++ tests/test_game_service.py
@@ -5,6 +5,11 @@ import pytest
 from app.domain.models import GameStatus, Player
 
 
+@pytest.fixture
+def game(service):
+    return service.create_game()
+
+
 def test_create_empty_game(service):
     # Act
     game = service.create_game()
@@ -23,3 +28,19 @@ def test_non_existing_id(service):
     # Act + Assert
     with pytest.raises(KeyError):
         service.get_game(id_inexistente)
+
+
+def test_valid_move(service, game):
+    # Act
+    update_game = service.make_move(game.id, Player.X, position=0)
+
+    # Assert
+    assert update_game.board[0] == "X"
+    assert update_game.current_player == Player.O
+
+
+def test_out_of_turn_player(service, game):
+    # Act + Assert
+    service.make_move(game.id, Player.X, position=0)
+    with pytest.raises(ValueError):
+        service.make_move(game.id, Player.X, position=1)
